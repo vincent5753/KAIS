0b460e79b19b57d851503f36d65422e389030ed9
VP <51seer.vincent@gmail.com>
2023-08-04 03:40:08 +0800

    Fix removal of docker and containerd properly :)
    Signed-off-by: VP <51seer.vincent@gmail.com>

--- nemesis.sh
+++ nemesis.sh
@@ -4,13 +4,16 @@ sudo kubeadm reset -f
 rm -rf ~/.kube
 sudo apt remove -y --allow-change-held-packages kubelet kubectl kubeadm
 sudo apt autoremove -y
-sudo dpkg -r docker-ce
-# sudo dpkg --purge docker-ce
-sudo dpkg -r docker-ce-cli
-# sudo dpkg --purge docker-ce-cli
-sudo dpkg -r containerd.io
-# sudo dpkg --purge containerd.io
+
+sudo systemctl stop docker.socket
+sudo systemctl stop docker.service
+sudo dpkg --purge docker-ce-cli docker-ce containerd.io
+
 sudo rm -rf /var/lib/docker /etc/docker
 sudo rm /etc/apparmor.d/docker
 sudo groupdel docker
 sudo rm -rf /var/run/docker.sock
+
+sudo systemctl stop containerd
+sudo dpkg --purge containerd.io
+sudo rm -rf /var/lib/containerd/
