949a99c90221f1f7a441ae306c4f8a8e8902de82
h124726398 <88424915+h124726398@users.noreply.github.com>
2023-10-01 16:59:34 +0800

    Update Ubuntu20-K8s_latest-containerd-flannel.sh

--- latest/Ubuntu20-K8s_latest-containerd-flannel.sh
+++ latest/Ubuntu20-K8s_latest-containerd-flannel.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-# By VP@23.08.17
 
 # Install bacsic packages
-
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg containerd.io
 
 # containerd
-
 sudo mkdir -p /etc/containerd
 sudo containerd config default | sudo tee /etc/containerd/config.toml
 sudo sed -i "s/SystemdCgroup = false/SystemdCgroup = true/g" /etc/containerd/config.toml
@@ -14,13 +11,11 @@ grep SystemdCgroup /etc/containerd/config.toml
 sudo systemctl restart containerd
 
 # add k8s repo
-
 sudo su -c "curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -"
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 sudo apt update
 
 # Essential Tweaks
-
 sudo swapoff -a
 cat << EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
@@ -40,21 +35,17 @@ sudo apt install -y kubelet kubectl kubeadm
 sudo apt-mark hold kubelet kubeadm kubectl
 sudo kubeadm init --service-cidr=10.96.0.0/12 --pod-network-cidr=10.244.0.0/16 --v=6
 
-### Copy Config
-
+# Copy Config
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-### Flannel CNI
-
+# Flannel CNI
 kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
 
-### Waiting until Ready
-
+# Waiting until Ready
 kubectl cluster-info
 watch -n 1 kubectl get nodes -o wide
 
-### Taint(if needed)
-
+# Taint(if needed)
 kubectl taint nodes --all node-role.kubernetes.io/control-plane-
