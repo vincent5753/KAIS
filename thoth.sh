dc7bdaf4b1a62dbefe0b9064d173efa3bf3632fa
VP <51seer.vincent@gmail.com>
2024-04-21 01:47:03 +0800

    Added platform detection in thoth.sh
    Signed-off-by: VP <51seer.vincent@gmail.com>

--- thoth.sh
+++ thoth.sh
@@ -20,7 +20,26 @@ case "${arch}" in
     echo "unknow arch"
     ;;
 esac
-# TDL Platform / Distro
+
+# Platform Detection
+# ref: https://stackoverflow.com/questions/394230/how-to-detect-the-os-from-a-bash-script
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  echo "On linux"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "On macOS"
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+  echo "A computer is like air conditioning – it becomes useless when you open..."
+elif [[ "$OSTYPE" == "msys" ]]; then
+ echo "A computer is like air conditioning – it becomes useless when you open..."
+elif [[ "$OSTYPE" == "win32" ]]; then
+  echo "A computer is like air conditioning – it becomes useless when you open..."
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+  echo "On freebsd"
+else
+  echo "Not supported OS?"
+fi
+
+# TDL: distro detection
 }
 
 os_detect
