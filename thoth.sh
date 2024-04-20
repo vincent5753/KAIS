d70bc743e3f931efe54e3037377d7c4e5cc6ae21
VP <51seer.vincent@gmail.com>
2024-04-20 23:17:15 +0800

    improved syntax of thoth.sh
    Signed-off-by: VP <51seer.vincent@gmail.com>

--- thoth.sh
+++ thoth.sh
@@ -6,14 +6,16 @@ arch=$(uname -m)
 echo "${arch}"
 
 case "${arch}" in
-    x86_64)
-        echo "amd64"
-        ;;
-    arm64)
-        echo "arm64"
-        ;;
-    aarch64)
-        echo "arm64"
-        ;;
-    *) echo "unknow arch"
+  x86_64)
+    echo "amd64"
+    ;;
+  arm64)
+    echo "arm64"
+    ;;
+  aarch64)
+    echo "arm64"
+    ;;
+  *)
+    echo "unknow arch"
+    ;;
 esac
