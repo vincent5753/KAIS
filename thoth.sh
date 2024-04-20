875cf9b2d2494a893c6e72f0f9e73b320b612492
VP <51seer.vincent@gmail.com>
2024-04-20 23:11:01 +0800

    Added arch detection in thoth.sh
    Signed-off-by: VP <51seer.vincent@gmail.com>

--- thoth.sh
+++ thoth.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 echo "[info] running preflight checks!"
+
+arch=$(uname -m)
+echo "${arch}"
+
+case "${arch}" in
+    x86_64)
+        echo "amd64"
+        ;;
+    arm64)
+        echo "arm64"
+        ;;
+    aarch64)
+        echo "arm64"
+        ;;
+    *) echo "unknow arch"
+esac
