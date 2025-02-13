d9f325dae3c990a5b7f4984d8e2ad764d4682b35
kanic1111 <daniel890723@gmail.com>
2025-02-13 14:00:26 +0800

    add containerd v2.0.2 install script

---
+++ beta/Ubuntu2204-K8s_v1-31-containerd-Cillum-multus.sh
@@ -0,0 +1,82 @@
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
+
+# Install containerd v2.0.2
+curl -Ol https://github.com/containerd/containerd/releases/download/v2.0.2/containerd-2.0.2-linux-amd64.tar.gz
+curl -Ol https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
+sudo tar Cxzvf /usr/local containerd-1.6.2-linux-amd64.tar.gz
+
+# Set Containerd to start via systemd
+sudo mv containerd.service /lib/systemd/system/containerd.service
+sudo systemctl daemon-reload
+sudo systemctl enable --now containerd
+
+# install runc
+curl -Ol https://github.com/opencontainers/runc/releases/download/v1.2.4/runc.amd64
+sudo install -m 755 runc.amd64 /usr/local/sbin/runc
+
+# install containderd CNI plugin
+curl -Ol https://github.com/containernetworking/plugins/releases/download/v1.6.2/cni-plugins-linux-amd64-v1.6.2.tgz
+sudo mkdir -p /opt/cni/bin
+sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.1.1.tgz
+
+# remove file after installed
+sudo rm *.tar.gz runc.amd64 *.tgz
+
+
+# Set SystemCgroup
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -i "s/SystemdCgroup = false/SystemdCgroup = true/g" /etc/containerd/config.toml
+grep SystemdCgroup /etc/containerd/config.toml
+sudo systemctl restart containerd
+
+
+# download kubernetes keyring
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+
+# Essential Tweaks
+sudo swapoff -a
+cat << EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+sudo modprobe overlay
+sudo modprobe br_netfilter
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+sudo sysctl --system
+
+# insall k8s (newest Stable version)
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+sudo kubeadm init --service-cidr=10.96.0.0/12 --pod-network-cidr=10.244.0.0/16 --v=6
+
+mkdir -p $HOME/.kube
+# Copy the kubeconfig file to the .kube directory
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+# Change ownership of the kubeconfig file to your user
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl taint node --all node-role.kubernetes.io/control-plane:NoSchedule-
+
+# set up autocomplete in bash into the current shell, bash-completion package should be installed first.
+source <(kubectl completion bash) 
+echo "source <(kubectl completion bash)" >> ~/.bashrc 
+sudo crictl config runtime-endpoint unix:///var/run/containerd/containerd.sock
+# Install cillum-cli
+CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
+CLI_ARCH=amd64
+if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
+curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum
+sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin
+rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+
+# install cillum with cni-exclusive OFF
+cilium install --version 1.16.5 --set cni.exclusive=false
