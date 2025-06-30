ac4f76160f8a20195a0059cdef60f1782b96caa5
VP <51seer.vincent@gmail.com>
2025-06-30 23:41:33 +0800

    Added info and error output
    Signed-off-by: VP <51seer.vincent@gmail.com>

--- legacy/Ubuntu2404-K8s_1_33-containerd-flannel.sh
+++ legacy/Ubuntu2404-K8s_1_33-containerd-flannel.sh
@@ -75,30 +75,30 @@ check_command_available(){
 install_apt_packages(){
 
     if [ "$#" -eq 0 ]; then
-        echo "Please provide at least one package name to install."
+        _error "Please provide at least one package name to install."
         return 1
     fi
 
-    echo "Updating package lists..."
+    _info "Updating package lists..."
     sudo apt-get update -y
 
-    echo "Installing specified packages..."
+    _info "Installing specified packages..."
     for package in "$@"; do
-        echo "Checking if ${package} is already callable..."
+        _info "Checking if ${package} is already callable..."
         if check_command_available "${package}"; then
-            echo "${package} is already callable. Skipping installation."
+            _info "${package} is already callable. Skipping installation."
         else
-            echo "Attempting to install: ${package}"
+            _info "Attempting to install: ${package}"
             sudo apt-get install -y "${package}"
             if [ $? -eq 0 ]; then
-                echo "${package} installed successfully."
+                _info "${package} installed successfully."
             else
-                echo "Failed to install ${package}. Please check the package name or your internet connection."
+                _error "Failed to install ${package}. Please check the package name or your internet connection."
             fi
         fi
     done
 
-    echo "Package installation process completed."
+    _info "Package installation process completed."
 
 }
 
@@ -106,7 +106,7 @@ install_apt_packages(){
 mark_apt_packages(){
 
     if [ "$#" -eq 0 ]; then
-        echo "Please provide at least one package name to mark."
+        _error "Please provide at least one package name to mark."
         return 1
     fi
 
@@ -117,30 +117,34 @@ mark_apt_packages(){
 
 install_docker_runtime(){
 
+    _info "Checking if iptables is installed"
     if check_command_available "iptables"
     then
-        echo "iptables is installed."
+        _info "iptables is installed."
     else
-        echo "iptables is NOT installed, installing it cuz docker-ce needs it."
+        _info "iptables is NOT installed, installing it cuz docker-ce needs it."
         install_apt_packages iptables
     fi
 
     base_url="https://download.docker.com/linux/ubuntu/dists/${OS_CODE_NAME}/pool/stable/${CPU_ARCH}"
 
+    _info "Download and installing debs from docker"
     for DEB in ${DOCKER_DEB[@]}
     do
+        _info "Download and installing ${DEB}"
         curl -s --create-dirs -o "${TEMP_DIR}/docker_debs/${DEB}" "${base_url}/$DEB"
         sudo dpkg -i "${TEMP_DIR}/docker_debs/${DEB}"
     done
 
     # containerd
+    _info "Setting up containerd"
     sudo mkdir -p /etc/containerd
     sudo containerd config default | sudo tee /etc/containerd/config.toml
     sudo sed -i "s/SystemdCgroup = false/SystemdCgroup = true/g" /etc/containerd/config.toml
     grep SystemdCgroup /etc/containerd/config.toml
     sudo systemctl restart containerd
 
-# change docker cgroup driver to systemd
+_info "Changing docker cgroup driver to systemd"
 cat <<EOF | sudo tee /etc/docker/daemon.json
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
@@ -153,7 +157,9 @@ cat <<EOF | sudo tee /etc/docker/daemon.json
 EOF
 
     sudo mkdir -p /etc/systemd/system/docker.service.d
+    _info "Adding current user to docker group"
     sudo usermod -aG docker $(logname)
+    _info "Restarting docker"
     sudo systemctl daemon-reload
     sudo systemctl enable docker
     sudo systemctl restart docker
@@ -164,6 +170,7 @@ EOF
 
 add_k8s_apt_repo(){
 
+    _info "Adding Kubernetes apt repo"
     curl -fsSL "https://pkgs.k8s.io/core:/stable:/v${K8S_VER}/deb/Release.key" | sudo gpg --yes --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
     echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v${K8S_VER}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
     sudo apt-get update
@@ -173,14 +180,18 @@ add_k8s_apt_repo(){
 
 do_k8s_tweaks(){
 
+    _info "Doing some system tweaks needed by kubertes"
+    _info "Disabling off swap"
     sudo swapoff -a
     sudo sed -i '/swap/s/^/#/' /etc/fstab
 cat << EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF
+    _info "Loading Linux Kernel Modules"
     sudo modprobe overlay
     sudo modprobe br_netfilter
+    _info "Writing sysctl configs"
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
@@ -193,6 +204,7 @@ EOF
 
 copy_kube_config(){
 
+    _info "Copy kube config to home directory"
     mkdir -p $HOME/.kube
     sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
     sudo chown $(id -u):$(id -g) $HOME/.kube/config
@@ -202,6 +214,7 @@ copy_kube_config(){
 
 remove_node_taint(){
 
+    _info "Remove taint from the node"
     kubectl taint nodes --all node-role.kubernetes.io/master-
     kubectl taint nodes --all node-role.kubernetes.io/control-plane-
 
